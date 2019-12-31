7033b819b02e112342cc7105aad0975e0c0c2623
mmeharigit <mmmehariwoldeab@gmail.com>
2019-12-31 17:37:19 +0300

    solomon bogale

--- ConsoleApp3/Program.vb
+++ ConsoleApp3/Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("Hello Mulugeta")
+        ' solomon bogale
     End Sub
 End Module
